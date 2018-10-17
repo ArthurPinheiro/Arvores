478896a942b2b6aae911fc839f72c9aab25e1fe7
Arthur Pinheiro de Lima <arthur.p@academico.ifrn.edu.br>
2018-10-16 22:55:52 -0300

    Adicionando métodos do MapaHash

--- Hash/Registro.java
+++ Hash/Registro.java
@@ -1,3 +1,42 @@
 public class Registro{
+
+    private int key;
+    private String value;
+    Registro proximo;
+
+    public Registro(){
+        this.proximo = null;
+    }
+
+    public Registro(int key, String value){
+        this.key = key;
+        this.value = value;
+        this.proximo = null;
+    }
+
+    public void setKey(int key){
+        this.key = key;
+    }
+    public int getKey(){
+        return this.key;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return this.value;
+    }
+
+    public void setProximo(Registro proximo){
+        this.proximo = proximo;
+    }
+    public Registro getProximo(){
+        return this.proximo;
+    }
     
+    public int hashCode(){
+        return key % 100;
+    }
+
 }
